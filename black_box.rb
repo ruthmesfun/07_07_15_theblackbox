5376466253d6d127f77c2283d754988c277cae1c
ruthmesfun <ruth.mesfun@gmail.com>
2015-07-07 09:45:47 +0000

    first commit

---
+++ black_box.rb
@@ -0,0 +1,15 @@
+# Create a method that gives the answer 546
+
+
+#Create a method that inputs two arguments and outputs 1234098
+
+#Create a method that inputs two arguments and outputs:
+#Dear Sarah Lee, I am so excited for you to come to my house on July 12th.
+
+
+#Create a method that inputs two arguments and outputs: The Cookie Batch has 5 boxes of cookies which is a total of 60 cookies.
+
+
+#Create a method that outputs: Alice is our 100th customer! 
+
+#Using what you learned yesterday and today, write a method that outputs someones favorite cookie and their name. But the user must input the information by being asked a question. (HINT: use the gets.chomp method and then use that variable in your own method)
